06285165082f34fdd43c934978731968d04416b0
jualiasha <jualiasha@gmail.com>
2023-07-17 01:36:11 +0300

    markedDown logic importing and parsing added

--- src/components/Chat.ts
+++ src/components/Chat.ts
@@ -1,45 +1,37 @@
-import { LitElement, css, html, PropertyValues, TemplateResult } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { css, html, LitElement, PropertyValues, TemplateResult } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { Marked } from "@ts-stack/markdown";
 
 @customElement("chat-helper")
 export class Chat extends LitElement {
-  @state()
-  private _introduction: TemplateResult = html`<p>
-      Звітайтесь, доброго дня/доброго вечора,
-    </p>
-    <p>
-      Мене звуть [Ваше ім'я], і я досвідчений юрист, спеціалізуюся в галузі
-      [галузі права]. Я дуже рада бути тут сьогодні і мати можливість
-      представити себе.
-    </p>
-    Я працюю юристом вже понад 15 років, і протягом своєї кар'єри мені пощастило
-    працювати з різноманітними клієнтами, включаючи фізичних осіб, корпорації та
-    організації. Мої компетенції лежать в [конкретних галузях права], де я
-    успішно вирішувала складні справи, надавала юридичні консультації та
-    досягала вигідних угод.
-    <p>
-      Те, що робить мене особливою як юриста, це моя нехитра відданість
-      досягненню найкращих можливих результатів для моїх клієнтів. Я підходжу до
-      кожної справи з ретельним дослідженням, уважним ставленням до деталей та
-      стратегічним мисленням. Я надто розумію важливість ефективної комунікації,
-      як з моїми клієнтами, так і з протилежною стороною, оскільки я розумію, що
-      будування сильних відносин сприяє позитивним вирішенням.
-    </p>
-    <p>
-      Поміж моїми юридичними навичками я пишаюся тим, що є співчутливим і
-      емпатичним захисником моїх клієнтів. Я намагаюся зрозуміти їх унікальні
-      потреби та стурбованості, і працюю наполегливо, щоб забезпечити
-      персоналізоване та ефективне юридичне представництво, зорієнтоване на їх
-      конкретні обставини.
-    </p>
-    <p>
-      Протягом всієї кар'єри я залишалась присвяченою оновленню своїх знань із
-      новітніх розробок у правовій сфері. Я регулярно відвідую юридичні
-      конференції, семінари та майстер-класи, і активно займаюся постійним
-      професійним розвитком, щоб забезпечити, що мої клієнти отримують юридичні
-      послуги найвищої якості. Я з нетерпінням чекаю можливості принести свої
-      компетенції.
-    </p> `;
+  /**
+   * Chat introduction text.
+   */
+  @property({ type: Object })
+  _introduction: TemplateResult = html``;
+
+  connectedCallback() {
+    super.connectedCallback();
+    const self = this;
+    import("../assets/content/chat-introduction.md").then((res) => {
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((text) => {
+          self._introduction = html`${Marked.parse(text)}`;
+        });
+    });
+  }
+
+  protected willUpdate(_changedProperties: PropertyValues) {
+    console.log(_changedProperties, this._introduction);
+    if (
+      _changedProperties.has("_introduction") &&
+      this._introduction.values[0]
+    ) {
+      this.type("#introduction", this._introduction);
+    }
+  }
+
   render() {
     return html`
       <div class="chat">
@@ -47,11 +39,6 @@ export class Chat extends LitElement {
         <div id="helper" class="block"></div>
       </div>
     `;
-  }
-
-  protected firstUpdated(_changedProperties: PropertyValues) {
-    // const introduction=this.renderRoot.querySelector('#introduction')
-    this.type("#introduction", this._introduction);
   }
 
   public type(selector: string, htmlTemplate: TemplateResult) {
@@ -62,7 +49,8 @@ export class Chat extends LitElement {
     const speed = 10;
     // debugger;
     const typeWriter = (): void => {
-      const typedString: string = htmlTemplate.strings[0];
+      // @ts-ignore
+      const typedString: string = htmlTemplate.values[0];
       // Check if all HTML template has been typed
       if (index < typedString.length && typingText) {
         // Append next character to the paragraph element
